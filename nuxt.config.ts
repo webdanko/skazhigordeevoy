55098be5f4dfdc299c5609c582b99c18b522659a
Elijah Danilov <danilov.komrad@ya.ru>
2024-03-09 02:03:31 +0200

    prerelease v0.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
-    "@nuxtjs/fontaine"
-  ],
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1.0',
-    }
-  },
-  css: [
-    '~/assets/fonts/proxima/stylesheet.css',
-  ],
-  image: {
-    inject: true
-  },
-  devtools: { enabled: true },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxtjs/fontaine", "nuxt-paypal"],
+    app: {
+        head: {
+            title: 'Фандрайзинг – «Скажи Гордеевой»',
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1.0',
+            link: [
+                {rel: 'apple-touch-icon', sizes: "180x180", href: ""},
+                {rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
+                {rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+                {rel: "manifest", href: "/site.webmanifest"}
+            ],
+            script: [
+                {src:"https://www.paypalobjects.com/donate/sdk/donate-sdk.js", defer: true}
+            ]
+        }
     },
-  },
+    css: [
+        '~/assets/fonts/proxima/stylesheet.css',
+    ],
+    image: {},
+    paypal: {
+        clientId: 'ASwuhBpJqSczQIY1cCD2qd3AgJ1KZ0amTiQYfvm7UsZlxpgxiD7REnt4on2kCwxgiU52bhDRiumgRI70',
+    },
+    devtools: {enabled: true}
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,78 @@
 /** @type {import('tailwindcss').Config} */
 
-// const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require( "tailwindcss/defaultTheme" );
+const plugin = require( "tailwindcss/plugin.js" );
 // const colors = require('tailwindcss/colors');
 
 module.exports = {
-  content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./app.vue",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    extends: {
-      fontFamily: {
-        'sans': ['"Proxima Nova"'],
-        'serif': ['"Proxima Nova"'],
-        'mono': ['"Proxima Nova"']
-      }
-    }
-  },
-  plugins: [],
+	content: [
+		"./components/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./app.vue",
+		"./nuxt.config.{js,ts}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				'boosty': {
+					'50': '#fef5ee',
+					'100': '#fde7d7',
+					'200': '#fbcbad',
+					'300': '#f8a779',
+					'400': '#f47843',
+					'500': '#f15f2c',
+					'600': '#e13c15',
+					'700': '#bb2b13',
+					'800': '#952417',
+					'900': '#782016',
+					'950': '#410d09',
+				},
+				'patreon': {
+					'50': '#f5f5f5',
+					'100': '#e8e8e8',
+					'200': '#d1d1d1',
+					'300': '#b0b0b0',
+					'400': '#878787',
+					'500': '#6e6e6e',
+					'600': '#5c5c5c',
+					'700': '#4f4f4f',
+					'800': '#454545',
+					'900': '#3d3d3d',
+					'950': '#000000',
+				},
+				'telegram': {
+					'50': '#f2f8fd',
+					'100': '#e3eefb',
+					'200': '#c1ddf6',
+					'300': '#8ac0ef',
+					'400': '#4ca0e4',
+					'500': '#2481cc',
+					'600': '#1768b2',
+					'700': '#145390',
+					'800': '#144778',
+					'900': '#163c64',
+					'950': '#0f2742',
+				},
+				'youtube': {
+					'50': '#fff0f0',
+					'100': '#ffdddd',
+					'200': '#ffc0c0',
+					'300': '#ff9494',
+					'400': '#ff5757',
+					'500': '#ff2323',
+					'600': '#ff0000',
+					'700': '#d70000',
+					'800': '#b10303',
+					'900': '#920a0a',
+					'950': '#500000',
+				},
+			},
+			fontFamily: {
+				'sans': ['"Proxima Nova"', ...defaultTheme["fontFamily"].sans],
+				'serif': ['"Proxima Nova"', ...defaultTheme["fontFamily"].serif],
+			}
+		}
+	}
 };
