8633cd759e7b672e955c4e5c31e42d448a430909
Elijah Danilov <danilov.komrad@ya.ru>
2024-03-09 05:00:59 +0200

    prerelease v0.3

--- app.vue
+++ app.vue
@@ -208,7 +208,7 @@ onMounted(() => {
 </template>
 
 <style lang="postcss">
-@import url("~/assets/css/styles.css");
+@import url("~assets/css/styles.css");
 
 html{
   font-size: 18px;
@@ -220,38 +220,6 @@ p{
 </style>
 
 <script>
-import {CopyClipboard} from "flowbite";
-
-const $defaultIcon = document.getElementById('default-icon');
-const $successIcon = document.getElementById('success-icon');
-
-const $defaultTooltipMessage = document.getElementById('default-tooltip-message');
-const $successTooltipMessage = document.getElementById('success-tooltip-message');
-
-CopyClipboard.prototype.updateOnCopyCallback((clipboard) => {
-  showSuccess();
-  // reset to default state
-  setTimeout(() => {
-    resetToDefault();
-  }, 2000);
-})
-//
-// const showSuccess = () => {
-//   $defaultIcon.classList.add('hidden');
-//   $successIcon.classList.remove('hidden');
-//   $defaultTooltipMessage.classList.add('hidden');
-//   $successTooltipMessage.classList.remove('hidden');
-//   tooltip.show();
-// }
-//
-// const resetToDefault = () => {
-//   $defaultIcon.classList.remove('hidden');
-//   $successIcon.classList.add('hidden');
-//   $defaultTooltipMessage.classList.remove('hidden');
-//   $successTooltipMessage.classList.add('hidden');
-//   tooltip.hide();
-// }
-
 export default {
   data(){
     return {
